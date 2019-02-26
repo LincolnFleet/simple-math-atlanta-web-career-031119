489f7ca2506a517999c05c5b97dfd96af1a9a165
Travis Martin <travisemerymartin@gmail.com>
2019-02-26 21:06:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,26 +1,26 @@
 def addition(num1, num2)
   sum = num1 + num2
-  sum
+  return sum
 end
 
 def subtraction(num1, num2)
   sum = num1 - num2
-  sum
+  return sum
 end
 
 def division(num1, num2)
   div = num1 / num2
-  div
+  return div
 end
 
 def multiplication(num1, num2)
   mult = num1 * num2
-  mult
+  return mult
 end
 
 def modulo(num1, num2)
   mod = num1 % num2
-  mod
+  return mod
 end
 
 def square_root(num)
@@ -30,4 +30,5 @@ end
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
   solve = num1 + ((num2 * num3) / num4)
+  return solve
 end
