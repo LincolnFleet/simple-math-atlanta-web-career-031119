455031fe3f323f14f6ffd8e22f76967ef37971ff
Travis Martin <travisemerymartin@gmail.com>
2019-02-26 21:03:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,9 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  div = num1 / num2
+  div
 end
 
 def multiplication(num1, num2)
+  mult = num1 * num2
+  mult
 end
 
 def modulo(num1, num2)
